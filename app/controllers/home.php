422cc8e0490f271a1498744441f53f2482044e2f
Trung-Silva <htsilvakt04@gmail.com>
2016-12-16 20:27:18 +0700

    building a MVC structure

---
+++ app/controllers/home.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ */
+class Home extends Controller
+{
+  public function index($params = "", $ortherName = "")
+  {
+    echo $params;
+    echo $ortherName;
+  }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -5,9 +5,38 @@
  */
 class App
 {
+  protected $controller = "home";
+  protected $method = "index";
+  protected $params = [
+
+  ];
 
   function __construct()
   {
-    echo "You are calling an App's Object";
+    $url = $this->parseUrl();
+    //Check if the method is provided then use that new method and unset it from the array;
+    if(file_exists("../app/controllers/{$url[0]}.php")) {
+      $this->controller = $url[0];
+      unset($url[0]);
+    }
+    require_once("../app/controllers/$this->controller.php");
+    $this->controller = new $this->controller;
+    if(isset($url[1])) {
+      if(method_exists($this->controller,$url[1])) {
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+    $this->params = $url ? array_values($url) : [];
+    call_user_func_array([$this->controller, $this->method], $this->params);
+
   }
+
+  public function parseUrl()
+  {
+    if(isset($_GET["url"])) {
+      return $url = explode("/", filter_var(rtrim($_GET["url"], "/"), FILTER_SANITIZE_URL));
+    }
+  }
+
 }
